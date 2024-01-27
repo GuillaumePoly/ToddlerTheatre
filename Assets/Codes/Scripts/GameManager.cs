6afdb79acfe3c3844fa4a85b24c4d966ff6cb489
GuillaumePoly <guillaumeppauli@gmail.com>
2024-01-27 12:43:21 +0100

    MEGA MERGE CONFICT

--- Assets/Codes/Scripts/GameManager.cs
+++ Assets/Codes/Scripts/GameManager.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public List<StoryPanel> panels;
     public List<Transform> mainCharacterMarkers;
-    
+    public GameObject endingPrefab;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
     public void PlayScene()
     {
         StartCoroutine("AnimateMainCharacter");
@@ -29,19 +40,30 @@ public class GameManager : MonoBehaviour
         foreach (var f in animateCharacterTo(character, thirdPanelPos.position)) yield return f;
 
         yield return new WaitForSeconds(2);
+
+        AddEnding("Happy Family");
     }
 
     IEnumerable animateCharacterTo(Character character, Vector3 target)
     {
+        var initialPos = character.transform.position;
+
         var elapsedTime = 0f;
         var waitTime = 3f;
         while (elapsedTime < waitTime)
         {
-            character.transform.position = Vector3.Lerp(character.transform.position, target, elapsedTime / waitTime);
+            character.transform.position = Vector3.Lerp(initialPos, target, elapsedTime / waitTime);
             elapsedTime += Time.deltaTime;
 
             // Yield here
             yield return null;
         }
+    }
+
+    void AddEnding(string description)
+    {
+        var ending = Instantiate(endingPrefab, new Vector3(0, 0, 10), Quaternion.identity);
+        var tmp = ending.GetComponentInChildren<TextMeshProUGUI>();
+        tmp.text = description;
     }
 }
