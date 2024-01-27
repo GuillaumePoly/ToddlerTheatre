eba9d22e795190fb04fa0da319cb6ffd659de572
Jesse Gielen <jessegielen@hotmail.com>
2024-01-27 03:01:16 +0100

    First prototype??

--- Assets/CharacterSelection.cs
+++ Assets/CharacterSelection.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class CharacterSelection : MonoBehaviour
 {
+    public GameManager gameManager;
     public List<Transform> markers;
 
     // Start is called before the first frame update
@@ -17,22 +18,30 @@ public class CharacterSelection : MonoBehaviour
     {
         if (!Input.GetMouseButtonDown(0)) return;
 
-            var point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            var hit = Physics2D.Raycast(point, Vector2.zero);
+        var point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var hit = Physics2D.Raycast(point, Vector2.zero);
 
-            if (hit.collider == null) return;
+        if (hit.collider == null) return;
 
-                var isCharacter = hit.collider.CompareTag("Player");
-                if (!isCharacter) return;
-
-                var character = hit.transform;
-
-                var freeMarker = markers.Where(m => m.childCount == 0).FirstOrDefault();
-
-                if (freeMarker != null)
-                {
-                    character.parent = freeMarker;
-                    character.position = freeMarker.position;
-                }
+        var isPlayButton = hit.collider.CompareTag("Finish");
+        if (isPlayButton) {
+            gameManager.PlayScene();
+            return;
         }
+
+        var isCharacter = hit.collider.CompareTag("Player");
+        if (!isCharacter) return;
+
+        var character = hit.transform;
+
+        var freeMarker = markers.Where(m => m.childCount == 0).FirstOrDefault();
+
+        if (freeMarker != null)
+        {
+            var characterScript = character.GetComponent<Character>();
+            freeMarker.parent.GetComponent<StoryPanel>().SetCharacter(characterScript.type);
+            character.parent = freeMarker;
+            character.position = freeMarker.position;
+        }
+    }
 }
