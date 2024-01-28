d285d7d1367a66676ca425c98694f01db1550d44
GuillaumePoly <guillaumeppauli@gmail.com>
2024-01-28 03:50:43 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/DragonFire.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonFire : MonoBehaviour
+{
+    public Animator m_Animator;
+
+    public void Fire()
+    {
+        m_Animator.SetBool("Fire", true);
+    }
+
+    public void StopFire()
+    {
+        m_Animator.SetBool("Fire", true);
+    }
+}
